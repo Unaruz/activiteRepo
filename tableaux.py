28c16ad3a5278c5141cd987771859f4e6c3361d6
unaruz <unaruz@gmail.com>
2018-08-20 23:19:56 +0200

    Ajout d'un tableau de string dans le fichier tableaux.py

--- tableaux.py
+++ tableaux.py
@@ -3,3 +3,9 @@ print(liste)
 print("Dernier élément du tableau")
 taille = len(liste)
 print(liste[taille - 1])
+
+# Création d un autre tableau de string
+nom = ["Jean", "Claire", "Emmanuel"]
+print(nom[0])
+print(nom[1])
+print(nom[2])
